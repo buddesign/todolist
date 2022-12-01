ca2c0ba4de5b290766fe59a60fea94fd309cfc32
buddesign <116799513+buddesign@users.noreply.github.com>
2022-12-01 11:53:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Text(props) {
+  return (
+    <div className="box-style">
+      <div>{props.text.text}</div>
+    </div>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "react를 배워봅시다." },
+    { id: 2, text: "react를 배워봅시다." },
+    { id: 3, text: "react를 배워봅시다." },
+  ]);
+
+  const [text, setText] = useState("");
+
+  const addHandler = () => {
+    const newTodos = {
+      id: todos.length + 1,
+      text: text,
+    };
+    setTodos([...todos, newTodos]);
+    console.log(todos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <p>TodoList</p>
+      <div className="input">
+        <input
+          className="add-input"
+          type="text"
+          value={text}
+          placeholder="자바스크립트를 배워봅시다."
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+        <button className="btn" onClick={addHandler}>
+          추가하기
+        </button>
+      </div>
+      <div className="box">
+        {todos.map((text) => {
+          return <Text text={text} key={text.id} />;
+        })}
+      </div>
     </div>
   );
 }
